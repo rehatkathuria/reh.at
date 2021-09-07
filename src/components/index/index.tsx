2c6e58c4cf600fed63b65d66e5db5035508b719c
Rehat Kathuria <hello@reh.at>
2021-09-07 19:20:47 +0100

    Wrap the arrow into an appropriate string

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -2,38 +2,45 @@ import { useState } from "react"
 import { useHotkeys } from "react-hotkeys-hook"
 import {
 	ArrowContainer,
-	Container, DateStyle, DateTimeContainer, Header,
+	Container,
+	DateStyle,
+	DateTimeContainer,
+	Header,
 	Menu,
-	MenuItem, Playback, PlaybackControl, TimeStyle
+	MenuItem,
+	Playback,
+	PlaybackControl,
+	TimeStyle
 } from "./styles"
 
-
 const formattedTime = () => {
-	return new Date().toLocaleTimeString(
-		"en-US",
-		{ hour: "2-digit", minute: "2-digit", second: "2-digit" }
-	)
+	return new Date().toLocaleTimeString("en-US", {
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+	})
 }
 
 enum MainMenu {
 	about = "about",
 	colophon = "colophon",
 	darkroom = "darkroom",
-	seafronts = "seafronts"
+	seafronts = "seafronts",
 }
 
 const main = [
 	MainMenu.about,
 	MainMenu.colophon,
 	MainMenu.darkroom,
-	MainMenu.seafronts
+	MainMenu.seafronts,
 ]
 
 export const Component = () => {
-	const date = new Date().toLocaleDateString(
-		"en-US",
-		{ year: 'numeric', month: 'short', day: 'numeric' }
-	)
+	const date = new Date().toLocaleDateString("en-US", {
+		year: "numeric",
+		month: "short",
+		day: "numeric",
+	})
 
 	const [selected, setSelected] = useState(MainMenu.about)
 	const [timeString, setTimeString] = useState(formattedTime())
@@ -42,37 +49,47 @@ export const Component = () => {
 		if (setTimeString !== undefined) setTimeString(formattedTime())
 	}, 1000)
 
-	useHotkeys("up", () => {
-		switch (selected) {
-			case MainMenu.about:
-				break
-			case MainMenu.colophon:
-				setSelected(MainMenu.about)
-				break
-			case MainMenu.darkroom:
-				setSelected(MainMenu.colophon)
-				break
-			case MainMenu.seafronts:
-				setSelected(MainMenu.darkroom)
-				break
-		}
-	}, { keydown: true }, [selected])
+	useHotkeys(
+		"up",
+		() => {
+			switch (selected) {
+				case MainMenu.about:
+					break
+				case MainMenu.colophon:
+					setSelected(MainMenu.about)
+					break
+				case MainMenu.darkroom:
+					setSelected(MainMenu.colophon)
+					break
+				case MainMenu.seafronts:
+					setSelected(MainMenu.darkroom)
+					break
+			}
+		},
+		{ keydown: true },
+		[selected],
+	)
 
-	useHotkeys("down", () => {
-		switch (selected) {
-			case MainMenu.about:
-				setSelected(MainMenu.colophon)
-				break
-			case MainMenu.colophon:
-				setSelected(MainMenu.darkroom)
-				break
-			case MainMenu.darkroom:
-				setSelected(MainMenu.seafronts)
-				break
-			case MainMenu.seafronts:
-				break
-		}
-	}, { keydown: true }, [selected])
+	useHotkeys(
+		"down",
+		() => {
+			switch (selected) {
+				case MainMenu.about:
+					setSelected(MainMenu.colophon)
+					break
+				case MainMenu.colophon:
+					setSelected(MainMenu.darkroom)
+					break
+				case MainMenu.darkroom:
+					setSelected(MainMenu.seafronts)
+					break
+				case MainMenu.seafronts:
+					break
+			}
+		},
+		{ keydown: true },
+		[selected],
+	)
 
 	return (
 		<>
@@ -82,7 +99,7 @@ export const Component = () => {
 					{main.map((element) => {
 						return element === selected ? (
 							<MenuItem key={element}>
-								<ArrowContainer> > </ArrowContainer>
+								<ArrowContainer> {">"} </ArrowContainer>
 								{element}
 							</MenuItem>
 						) : (
