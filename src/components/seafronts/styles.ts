6964dc7914d10650c0be11222b332643fcd5603c
Rehat Kathuria <hello@reh.at>
2021-09-08 22:34:38 +0100

    Fix the seafronts content in the middle

--- src/components/seafronts/styles.ts
+++ src/components/seafronts/styles.ts
@@ -1,13 +1,10 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-	height: 100vh;
-	overflow-y: scroll;
-	padding: 36vh 0 36vh 0;
 	width: 500px;
 	::-webkit-scrollbar {
 		display: none;
 	}
-	-ms-overflow-style: none; /* IE and Edge */
-	scrollbar-width: none; /* Firefox */
+	-ms-overflow-style: none;
+	scrollbar-width: none;
 `
