c177ffc55d80b81bd78dcbbb328794fdf659e5fb
Rehat Kathuria <hello@reh.at>
2021-09-08 22:43:26 +0100

    Tweak colophon

--- src/components/colophon/index.tsx
+++ src/components/colophon/index.tsx
@@ -6,13 +6,18 @@ export const Component: FC = () => {
 	return (
 		<Container>
 			<Blurb>
-				This website was designed and built by me,{" "}
-				<BlurbHighlighted>Rehat Kathuria</BlurbHighlighted>, in London, England.
+				This website was designed and built by Rehat Kathuria, in London,
+				England.
 			</Blurb>
 			<P />
 			<Blurb>
-				The hardware and software used consists of a Macbook Air, Visual Studio
-				Code, GIT and Yarn.
+				Built upon the work of many open-source projects, including: Next.js,
+				P5, React, Styled Components, Typescript and Webpack.
+			</Blurb>
+			<P />
+			<Blurb>
+				There are no analytic trackers on this website and there{" "}
+				<BlurbHighlighted>never</BlurbHighlighted> will be.
 			</Blurb>
 			<P />
 			<Blurb>
@@ -20,16 +25,6 @@ export const Component: FC = () => {
 				either a Rolleiflex Type 5 or a Leica MA. The lenses on the cameras
 				consist of a Carl Zeiss Planar 75mm ƒ/3.5 and a Summaron-M 28mm ƒ/5.6,
 				respectively.
-			</Blurb>
-			<P />
-			<Blurb>
-				This website is built upon the work of many open-source projects,
-				including: Next.js, P5, React, Styled Components, Typescript and
-				Webpack.
-			</Blurb>
-			<P />
-			<Blurb>
-				There are no analytic trackers on this website and there never will be.
 			</Blurb>
 			<P />
 			<Blurb>
