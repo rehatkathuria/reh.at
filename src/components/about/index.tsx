986a9664c9b4af17a706fcb2eca4f0457853f781
Rehat Kathuria <hello@reh.at>
2021-09-09 09:17:39 +0100

    Punctuation

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -31,7 +31,7 @@ export const Component: FC = () => {
 				<Loop>
 					{ingredients.map((item) => (
 						<div key={item}>
-							<BlurbHighlighted>{item}</BlurbHighlighted>
+							<BlurbHighlighted>{item}.</BlurbHighlighted>
 						</div>
 					))}
 				</Loop>
