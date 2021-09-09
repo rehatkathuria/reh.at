3e6a295234ecd1c308b92a5b25ec877e46169bec
Rehat Kathuria <hello@reh.at>
2021-09-09 23:03:00 +0100

    Bump up the size of the videos

--- src/components/video/styles.ts
+++ src/components/video/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
 export const Video = styled.video`
-	height: 475px;
-	width: 475px;
+	height: 495px;
+	width: 495px;
 	border: 5px solid white;
 	margin: 10px 0 10px 0;
 	@media only screen and (max-width: 650px) {
