39f4a7972230de70ecb056da191f432667b56fb9
Rehat Kathuria <hello@reh.at>
2021-09-10 11:46:19 +0100

    Amend the viewport-fit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,9 +26,9 @@ const App = ({ Component, pageProps, router }: AppProps) => {
 			<Head>
 				<title>rehat</title>
 				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0'
-					lang='en'
+					name="viewport"
+					content="width=device-width, initial-scale=1.0 viewport-fit=cover"
+					lang="en"
 				/>
 			</Head>
 			<ThemeProvider theme={value ? Themes.Dark : Themes.Light}>
