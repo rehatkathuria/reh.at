4fe1cdd9b89c5cadcf414d02a854e4b1263d0155
Rehat Kathuria <hello@reh.at>
2021-09-10 09:26:06 +0100

    Lift up content embedded inside paragraphs and add silver halide component to the top

--- src/components/darkroom/styles.ts
+++ src/components/darkroom/styles.ts
@@ -1,13 +1,20 @@
 import { grain } from "@components/grain"
 import styled, { keyframes } from "styled-components"
 import { ContentContainer } from "@components/index/styles"
+import { Canvas as ThreeCanvas } from "@react-three/fiber"
+
+export const Canvas = styled(ThreeCanvas)`
+	background: ${(props) => props.theme.background};
+	max-height: 300px;
+	width: calc(100vw - 500px);
+`
 
 export const Container = styled(ContentContainer)`
 	height: 100vh;
 	overflow: scroll;
-	padding: 36vh 0 12vh 0;
+	padding: 0vh 0 12vh 0;
 	@media only screen and (max-width: 500px) {
 		width: 100vw;
-		padding: 18vh 20px 18vh 20px;
+		padding: 0vh 20px 18vh 20px;
 	}
 `
